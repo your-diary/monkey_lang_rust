c16ae9a96e9f04bd2dfb6a2932c7afba84b76ea6
ynn <contact@your-diary.io>
2022-08-03 19:40:01 +0900

    Refactored.

--- src/ast.rs
+++ src/ast.rs
@@ -1,180 +1,156 @@
-// use std::any::Any;
-// 
-// use super::token::Token;
-// 
-// /*-------------------------------------*/
-// 
-// pub trait Node {
-//     fn get_literal(&self) -> Option<String>;
-//     fn as_any(&self) -> &dyn Any;
-// }
-// 
-// pub trait Statement: Node {}
-// 
-// pub trait Expression: Node {}
-// 
-// /*-------------------------------------*/
-// 
-// pub struct Root {
-//     statements: Vec<Box<dyn Statement>>,
-// }
-// 
-// impl Node for Root {
-//     fn get_literal(&self) -> Option<String> {
-//         if (self.statements.is_empty()) {
-//             self.statements[0].get_literal()
-//         } else {
-//             None
-//         }
-//     }
-//     fn as_any(&self) -> &dyn Any {
-//         self
-//     }
-// }
-// 
-// impl Root {
-//     pub fn new() -> Self {
-//         Root {
-//             statements: Vec::new(),
-//         }
-//     }
-//     pub fn statements(&self) -> &Vec<Box<dyn Statement>> {
-//         &self.statements
-//     }
-//     pub fn statements_mut(&mut self) -> &mut Vec<Box<dyn Statement>> {
-//         &mut self.statements
-//     }
-// }
-// 
-// /*-------------------------------------*/
-// 
-// pub struct Identifier {
-//     token: Token,
-//     value: String,
-// }
-// 
-// impl Node for Identifier {
-//     fn get_literal(&self) -> Option<String> {
-//         self.token.literal().clone()
-//     }
-//     fn as_any(&self) -> &dyn Any {
-//         self
-//     }
-// }
-// 
-// impl Expression for Identifier {}
-// 
-// impl Identifier {
-//     pub fn new(token: Token, value: String) -> Self {
-//         Identifier { token, value }
-//     }
-//     pub fn token(&self) -> &Token {
-//         &self.token
-//     }
-//     pub fn value(&self) -> &str {
-//         &self.value
-//     }
-// }
-// 
-// /*-------------------------------------*/
-// 
-// pub struct LetStatement {
-//     token: Token,
-//     left: Identifier,
-//     right: Box<dyn Expression>,
-// }
-// 
-// impl Node for LetStatement {
-//     fn get_literal(&self) -> Option<String> {
-//         self.token.literal().clone()
-//     }
-//     fn as_any(&self) -> &dyn Any {
-//         self
-//     }
-// }
-// 
-// impl Statement for LetStatement {}
-// 
-// impl LetStatement {
-//     pub fn new(left: Identifier, right: Box<dyn Expression>) -> Self {
-//         LetStatement {
-//             token: Token::new(TokenType::Let, None),
-//             left,
-//             right,
-//         }
-//     }
-//     pub fn token(&self) -> &Token {
-//         &self.token
-//     }
-//     pub fn left(&self) -> &Identifier {
-//         &self.left
-//     }
-//     pub fn right(&self) -> &dyn Expression {
-//         self.right.as_ref()
-//     }
-// }
-// 
-// /*-------------------------------------*/
-// 
-// pub struct ReturnStatement {
-//     token: Token,
-//     value: Box<dyn Expression>,
-// }
-// 
-// impl Node for ReturnStatement {
-//     fn get_literal(&self) -> Option<String> {
-//         self.token.literal().clone()
-//     }
-//     fn as_any(&self) -> &dyn Any {
-//         self
-//     }
-// }
-// 
-// impl Statement for ReturnStatement {}
-// 
-// impl ReturnStatement {
-//     pub fn new(value: Box<dyn Expression>) -> Self {
-//         ReturnStatement {
-//             token: Token::new(TokenType::Return, None),
-//             value,
-//         }
-//     }
-//     pub fn token(&self) -> &Token {
-//         &self.token
-//     }
-//     pub fn value(&self) -> &dyn Expression {
-//         self.value.as_ref()
-//     }
-// }
-// 
-// /*-------------------------------------*/
-// 
-// pub struct ExpressionStatement {
-//     token: Token, //first token of an expression
-//     value: Box<dyn Expression>,
-// }
-// 
-// impl Node for ExpressionStatement {
-//     fn get_literal(&self) -> Option<String> {
-//         self.token.literal().clone()
-//     }
-//     fn as_any(&self) -> &dyn Any {
-//         self
-//     }
-// }
-// 
-// impl Statement for ExpressionStatement {}
-// 
-// impl ExpressionStatement {
-//     pub fn new(token: Token, value: Box<dyn Expression>) -> Self {
-//         ExpressionStatement { token, value }
-//     }
-//     pub fn token(&self) -> &Token {
-//         &self.token
-//     }
-//     pub fn value(&self) -> &dyn Expression {
-//         self.value.as_ref()
-//     }
-// }
-// 
-// /*-------------------------------------*/
+use std::any::Any;
+
+use super::token::Token;
+
+/*-------------------------------------*/
+
+pub trait Node {
+    fn as_any(&self) -> &dyn Any;
+}
+
+pub trait StatementNode: Node {}
+
+pub trait ExpressionNode: Node {}
+
+/*-------------------------------------*/
+
+pub struct RootNode {
+    statements: Vec<Box<dyn StatementNode>>,
+}
+
+impl Node for RootNode {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl RootNode {
+    pub fn new() -> Self {
+        RootNode {
+            statements: Vec::new(),
+        }
+    }
+    pub fn statements(&self) -> &Vec<Box<dyn StatementNode>> {
+        &self.statements
+    }
+    pub fn statements_mut(&mut self) -> &mut Vec<Box<dyn StatementNode>> {
+        &mut self.statements
+    }
+}
+
+/*-------------------------------------*/
+
+pub struct IdentifierNode {
+    token: Token,
+}
+
+impl Node for IdentifierNode {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl ExpressionNode for IdentifierNode {}
+
+impl IdentifierNode {
+    pub fn new(token: Token) -> Self {
+        IdentifierNode { token }
+    }
+    pub fn token(&self) -> &Token {
+        &self.token
+    }
+}
+
+/*-------------------------------------*/
+
+pub struct LetStatementNode {
+    token: Token,
+    left: IdentifierNode,
+    right: Box<dyn ExpressionNode>,
+}
+
+impl Node for LetStatementNode {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl StatementNode for LetStatementNode {}
+
+impl LetStatementNode {
+    pub fn new(left: IdentifierNode, right: Box<dyn ExpressionNode>) -> Self {
+        LetStatementNode {
+            token: Token::Let,
+            left,
+            right,
+        }
+    }
+    pub fn token(&self) -> &Token {
+        &self.token
+    }
+    pub fn left(&self) -> &IdentifierNode {
+        &self.left
+    }
+    pub fn right(&self) -> &dyn ExpressionNode {
+        self.right.as_ref()
+    }
+}
+
+/*-------------------------------------*/
+
+pub struct ReturnStatementNode {
+    token: Token,
+    value: Box<dyn ExpressionNode>,
+}
+
+impl Node for ReturnStatementNode {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl StatementNode for ReturnStatementNode {}
+
+impl ReturnStatementNode {
+    pub fn new(value: Box<dyn ExpressionNode>) -> Self {
+        ReturnStatementNode {
+            token: Token::Return,
+            value,
+        }
+    }
+    pub fn token(&self) -> &Token {
+        &self.token
+    }
+    pub fn value(&self) -> &dyn ExpressionNode {
+        self.value.as_ref()
+    }
+}
+
+/*-------------------------------------*/
+
+pub struct ExpressionStatementNode {
+    token: Token, //first token of an expression
+    value: Box<dyn ExpressionNode>,
+}
+
+impl Node for ExpressionStatementNode {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl StatementNode for ExpressionStatementNode {}
+
+impl ExpressionStatementNode {
+    pub fn new(token: Token, value: Box<dyn ExpressionNode>) -> Self {
+        ExpressionStatementNode { token, value }
+    }
+    pub fn token(&self) -> &Token {
+        &self.token
+    }
+    pub fn value(&self) -> &dyn ExpressionNode {
+        self.value.as_ref()
+    }
+}
+
+/*-------------------------------------*/
